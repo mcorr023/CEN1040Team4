6b2209f1041dfd1f709cc77d785307d8bccb7fbd
fcoll018 <98793578+fcoll018@users.noreply.github.com>
2022-04-15 18:14:20 -0400

    Add files via upload

---
+++ ratecomment/CommentController.java
@@ -0,0 +1,33 @@
+package com.example.demo.ratecomment;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping (path = "api/v1/comment")
+
+public class CommentController {
+
+    private final CommentService commentservice;
+
+    @Autowired
+    public CommentController(CommentService commentservice) {
+        this.commentservice = commentservice;
+    }
+    @GetMapping
+    public List<Comments> getComments () {
+        return CommentService.getComments();
+    }
+
+    @PostMapping
+    public void addNewComment (@RequestBody Comments comment){
+        CommentService.addNewComment(comment);
+    }
+
+    @DeleteMapping (path = "{commentId}")
+    public void deleteComment (@PathVariable("commentId") Long commentId){
+        CommentService.deleteComment(commentId);
+    }
+}
